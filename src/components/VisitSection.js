a7ef44ca52568879827fb2c510b3af47fe83ab64
knvj <97109617+knvj@users.noreply.github.com>
2024-04-30 17:05:10 +0200

    liitle changes at work

--- src/components/VisitSection.js
+++ src/components/VisitSection.js
@@ -4,14 +4,16 @@ import './VisitSection.css'
 function VisitSection() {
   return (
     <div id='visitSection' className='visit'>
-       <div className='visit-wrapper'>
-        <div className='visit-image'>
-                <img src='images/alfred-kubin.jpg' alt='/' />
-            </div>
+        <div className='visit-wrapper'>
             <div className='visit-text'>
                 Alfred Kubin Museum of Art: A World of Unease and Fantasy
-                </div>
-       </div>
+            </div>
+            <div className='visit-image'>
+                <img src='images/alfred-kubin.jpg' alt='/'/>
+                <img src='images/the-last-king.jpg' alt='/'/>
+            </div>
+
+        </div>
     </div>
   )
 }
